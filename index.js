bdddf573c0267ac0669e7779960ce132708ff599
Manu <84148859+JoaoManoelFontes@users.noreply.github.com>
2021-07-19 09:01:38 -0300

    Bot Browser

---
+++ index.js
@@ -0,0 +1,236 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const axios = require('axios');
+const jsdom = require('jsdom');
+const  {  JSDOM  }  =  jsdom ;
+
+const client = new Discord.Client({
+ presence: {
+  status: 'online',
+  activity: {
+   name: 'Pica-Pau',
+   type: 'WATCHING',
+  },
+ },
+});
+
+const prefix = "!"
+
+client.once('ready', () => {
+
+  console.log('bot logado em '+client.channels.cache.size+" "+'canais de '+client.guilds.cache.size+' servers') 
+
+  }); 
+ 
+
+client.on("message", function(message) {    
+  if (message.author.bot) return;
+
+    const errorEmbed = new Discord.MessageEmbed()
+  .setColor('#3C4043')
+  .setTitle('Erro!')
+  .setTimestamp()
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+  const idUrl = message.guild.id
+
+
+    if (command === "src"){
+
+ const dataSrc = axios({
+
+  url: "https://br.search.yahoo.com/search?p="+message.content.substring(5,message.content.length).replace(/ /g, "+")
+}).then((result) =>{
+
+  const found = (result.data.match(/<ol class=" reg searchCenterMiddle">.+<\/ol/))
+  const dom = new JSDOM(found)
+  const searchLink =(dom.window.document.getElementsByTagName('a'))
+  const imgFound =(dom.window.document.getElementsByTagName('img'))
+  const useless = []
+  const resul = []
+  const resultext = []
+
+
+  for (var i = 0; i <= searchLink.length-1; i++) {// removendo os links "desnecessários" para sua pesquisa
+
+    if(searchLink[i].href.startsWith('https://br.search.yahoo.com/')){
+      useless.push(searchLink[i].href)
+
+    }else if(searchLink[i].href.startsWith('https://br.images.search.yahoo.com/search')){
+      useless.push(searchLink[i].href)
+
+    }else if (searchLink[i].href.startsWith('https://br.news.search.yahoo.com/search')){
+      useless.push(searchLink[i].href)
+
+    }else if (searchLink[i].href.startsWith('https://br.video.search.yahoo.com/search')){
+      useless.push(searchLink[i].href)
+
+      }else if (searchLink[i].href.startsWith('https://cc.bingj.com/cache')){
+      useless.push(searchLink[i].href)
+
+        }else if (searchLink[i].href.startsWith('https://br.ajuda.yahoo.com/')){
+      useless.push(searchLink[i].href)
+
+       }else if (searchLink[i].href.startsWith('https://r.search.yahoo.com/')){
+      useless.push(searchLink[i].href)
+
+         }else if (searchLink[i].href.startsWith('https://esportes.yahoo.com/noticias/')){
+      useless.push(searchLink[i].href)
+
+    }else{
+      resul.push(searchLink[i].href)
+      resultext.push(searchLink[i].text)
+    }
+  }
+
+      for (var i = 0; i <= resultext.length-1; i++) {// caso não tenha um resumo abaixo do link
+        if(resultext[i]===""){
+          resultext.splice(i,1,"-esse site não apresenta nenhum resumo-")
+        }
+      }
+
+      if(resul[0]===undefined){ //se não for possível achar os links referentes à pesquisa
+
+        const searchEmbed = new Discord.MessageEmbed()
+        .setColor('#4B0082')
+        .setTitle('Resultados da pesquisa')
+        .addFields(
+            {name:"erro",value:"não foi possível encontrar resultados para essa pesquisa"},
+         )
+        .setTimestamp()        
+
+        message.channel.send(searchEmbed);
+
+      }else{
+
+        if(imgFound.length>=0){//se tiver uma imagem sobre a pesquisa na página em questão
+
+          const searchEmbed = new Discord.MessageEmbed()
+
+           .setColor('#4B0082')
+           .setTitle('Resultados da pesquisa')
+           .setDescription('resultados sobre '+message.content.substring(5,message.content.length).replace(/ /g, " "))
+           .addFields(
+              {name: resul[0],value:resultext[0]},
+              {name: resul[1],value:resultext[1]}
+           )
+           .addFields(
+            {name:"imagem sobre "+message.content.substring(5,message.content.length).replace(/ /g, " "),value:imgFound[0].title}
+          )
+           .setImage(imgFound[0].src)
+           .setTimestamp()
+  
+        message.channel.send(searchEmbed);   
+        
+        }else{
+
+      const searchEmbed = new Discord.MessageEmbed()
+        .setColor('#4B0082')
+        .setTitle('Resultados da pesquisa')
+        .setDescription('resultados sobre '+message.content.substring(5,message.content.length).replace(/ /g, " "))
+        .addFields(
+            {name: resul[0],value:resultext[0]},
+            {name: resul[1],value:resultext[1]}
+         )
+        .setTimestamp()
+
+      message.channel.send(searchEmbed);   
+        }
+      } 
+
+    }).catch((error) =>{
+      console.log(error)
+      })
+
+
+  }
+
+  else if(command === "watch"){
+
+    const dataWatch = axios({
+
+      url: "https://br.video.search.yahoo.com/search/video;_ylt=A2KLfRhB7upgluUATjPz6Qt.;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p="+message.content.substring(7,message.content.length).replace(/ /g, "+")
+    }).then((result) =>{
+
+      const videoFound = (result.data.match(/<ol><li class="vr vres">.+<\/ol/))
+      const videoDom = new JSDOM(videoFound)
+      const link = videoDom.window.document.getElementsByTagName("a")
+      const thumb = videoDom.window.document.getElementsByTagName("img")
+      const contentTitle = videoDom.window.document.getElementsByTagName("h3")
+      const contentTime = videoDom.window.document.getElementsByClassName("v-time")
+
+      if(link[0].href.startsWith("/video/play?p")){ //caso o vídeo for do YouTube
+
+          const dataYt = axios({  //vai no link do video pelo Yahoo e pega o link do video no YouTube
+
+            url: "https://br.video.search.yahoo.com/"+link[0].href
+          }).then((result) =>{
+            const yt = new JSDOM(result.data)
+            const watchInYt = yt.window.document.getElementsByClassName("url")
+          
+              const videoEmbed = new Discord.MessageEmbed()
+              .setColor('#FF0000')
+              .setAuthor('www.youtube.com','https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-5-2.png' ,'https://www.youtube.com/')
+              .setTitle('Resultados da pesquisa:')
+              .setDescription('video sobre '+message.content.substring(7,message.content.length).replace(/ /g, " "))
+              .setImage(thumb[0].src)
+              .addFields(
+                {name:contentTitle[0].textContent, value:watchInYt[0].href, inline:true}
+               )
+              .setFooter("duração: "+contentTime[0].textContent)
+    
+            message.channel.send(videoEmbed);
+              
+      }).catch((error) =>{
+        console.log(error)
+        })      
+
+      }else{
+
+        const videoEmbed = new Discord.MessageEmbed()
+        .setColor('#202A54')
+        .setAuthor('Site desconhecido')
+        .setTitle('Resultados da pesquisa:')
+        .setDescription('video sobre '+message.content.substring(7,message.content.length).replace(/ /g, " "))
+        .setImage(thumb[0].src)
+        .addFields(
+           {name:contentTitle[0].textContent, value:link[0].href}
+           )
+        .setFooter("informações tiradas inteiramente do Yahoo Vídeos")
+  
+      message.channel.send(videoEmbed);   
+       }
+    
+  }).catch((error) =>{
+    console.log(error)
+    })
+  
+  }
+
+  else if(command === "comandos"){
+  const commandEmbed = new Discord.MessageEmbed()
+
+   .setColor('#B35A33')
+   .setTitle('Menu de comandos do Bot Lampião')
+   .setAuthor('João Manoel', null , 'https://github.com/JoaoManoelFontes')
+   .setDescription('todos os comandos do bot aqui')
+   .addFields(
+     {name:"prefix: ",value:"!", inline:true},
+     {name:"library: ", value:"discord.js", inline:true},
+     {name:"version: ", value:"2.5.0", inline:true}
+   )
+   .addFields(
+     {name:'google', value:'O bot retorna um link que redireciona voçê ao google com sua pesquisa' },
+     {name:'src', value:'O bot retorna o primeiro resultado da sua pesquisa de acordo com o search do Yahoo. Obs: Tente fazer uma pesquisa simples para evitar erros'},
+     {name:'watch', value:'O bot retorna o primeiro resultado da sua pesquisa no Yahoo Videos, que geralmente é um vídeo do YouTube, por isso a mensagem personalizada'}
+     )
+   .setTimestamp()
+
+message.channel.send(commandEmbed);
+  }
+
+});
+
+client.login(config.BOT_TOKEN);
---
+++ readme.md
@@ -0,0 +1,62 @@
+<<h1 align="center">Bot Browser</h1>
+<h3 align="center">Bot do discord que tem a função de fazer a pesquisa em um browser por você </h3>
+<br/>
+<br/>
+<br/>
+<p align="center">Funções:</p>
+
+<ul> 
+
+<li>!src</li>
+ Pesquisa no yahoo o que o usuário pedir e manda no chat os 2 primeiros resultados junto com um pequeno resumo  e uma imagem sobre o link, todos tirados da página da pesquisa.
+<br/>
+<h4>Obs:</h4>
+<ul>
+<li>Talvez alguns desses links não tenham um resumo ou uma imagem (em ambos os casos o Bot avisará)</li>
+</ul>
+<br/>
+
+<h4>Exemplo:</h4>
+<ul>
+<li> !src github // o Bot vai pesquisar github no yahoo, vai pegar os links dos 2 primeiros resultados e mandar para o usuário </li>
+</ul>
+
+<br/>
+<br/>
+
+<li>!watch</li>
+Pesquisa o vídeo que o usuário pedir pelo Yahoo Vídeos e mostra o link, o título e a thumbnail do primeiro resultado.
+<br/>
+<h4>Obs:</h4>
+<ul>
+<li>Geralmente o resultado é um vídeo do YouTube, nesse caso o Bot enviará uma embed personalizada do site, mostrando também a duração do vídeo. Caso não seja do YouTube o Bot, além de mandar que é um site que ele não conhece, irá mostrar o link do site junto com a thumb e o título em uma embed geral</li>
+</ul>
+<br/>
+
+<h4>Exemplo:</h4>
+<ul>
+<li> !watch github // o Bot vai pesquisar github no Yahoo Vídeos, e vai mandar o primeiro resultado para o usuário </li>
+</ul>
+<br/>
+<br/>
+</ul>
+
+<br/>
+<h3 align="center">Bibliotecas:</h3>
+<br/>
+<br/>
+<dl>
+<dt>Axios</dt>
+<dd>Foi feita para extrair dados de outras páginas da internet, ou seja, Web Scraping. Usada no Bot Browser para pegar todo o html de uma página </dd>
+<dt>jsdom</dt>
+<dd>Biblioteca que consegue lê html e, consequentemente, criar váriaveis com as informações contidas no html em questão, como por exemplo armazenar os links dos primeiros resultados de uma página em uma variavel, e foi para isso que essa biblioteca foi usada no Bot. </dd>
+</dl>
+<br/>
+<br/>
+<p>O Bot Random foi feito com a library discord.js na versão 12.5.3 .</p>
+<p>Para adicionar esse codigo no seu bot, antes você terá que baixar o node js e, em seguida, essa biblioteca pelo npm.
+<p><a href="https://www.digitalocean.com/community/tutorials/how-to-build-a-discord-bot-with-node-js-pt">Esse site</a> explica passo-a-passo uma maneira de criar um bot no discord usando javascript.</p>
+<br/>
+<br/>
+<p>Para relatar qualquer problema ou tirar uma dúvida sobre o projeto:<p>
+<p align="right">Meu instagram: <a href="https://www.instagram.com/fontesjoaomanoel/">@fontesjoaomanoel
